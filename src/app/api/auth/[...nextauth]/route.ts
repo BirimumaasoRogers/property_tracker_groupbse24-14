2ec423e5d963da5c28b1e2ab39759f1b3d9a3327
Birimumaaso Rogers <birimumaaso14roja@gmail.com>
2024-10-14 23:06:44 +0300

    Update Options

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,50 +1,5 @@
-import NextAuth from 'next-auth';
-import CredentialsProvider from 'next-auth/providers/credentials';
-
-export const authOptions = {
-  providers: [
-    CredentialsProvider({
-      name: 'Credentials',
-      credentials: {
-        email: { label: 'Email', type: 'email', placeholder: 'you@example.com' },
-        password: { label: 'Password', type: 'password' },
-      },
-      async authorize(credentials) {
-        // Dummy user check
-        const user = { id: "1", name: "John Doe", email: "john.doe@example.com" } as const;
-
-        if (credentials && 
-          credentials?.email === "john.doe@example.com" &&
-          credentials?.password === "password123"
-        ) {
-          return user; // Return user if credentials match
-        } else {
-          return null; // Return null if credentials don't match
-        }
-      },
-
-    }),
-  ],
-  pages: {
-    signIn: '/dashboard/login', // Set custom login page route
-  },
-  session: {
-    strategy: 'jwt' as 'jwt' | 'database',
-  },
-  callbacks: {
-    async jwt({ token, user }: { token: any; user: any }) {
-      if (user) {
-        token.id = user.id;
-      }
-      return token;
-    },
-    async session({ session, token }: { session: any; token: any }) {
-      session.user.id = token.id;
-      return session;
-    },
-  },
-  secret: process.env.NEXTAUTH_SECRET, // Store securely in .env.local
-};
+import authOptions from '@/utilities/options';
+import NextAuth from 'next-auth/next';
 
 const handler = NextAuth(authOptions);
 
