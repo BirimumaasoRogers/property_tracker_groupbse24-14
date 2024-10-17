8b87dbadad7e34257e2df2dbd0f463962a4d0a2e
Rogers Birimumaaso <birimumaaso.rogers@students.mak.ac.ug>
2024-10-17 15:51:03 +0300

    Update vercel errors

--- src/app/dashboard/login/page.tsx
+++ src/app/dashboard/login/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { signIn } from "next-auth/react";
-import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
