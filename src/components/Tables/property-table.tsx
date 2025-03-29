1742fbb1a5d7376312b2292f966bc9ba11161797
Birimumaaso Rogers <birimumaaso14roja@gmail.com>
2025-03-29 21:47:34 +0300

    Update dashboard/property

--- src/components/Tables/property-table.tsx
+++ src/components/Tables/property-table.tsx
@@ -187,6 +187,7 @@ const columns: ColumnDef<Item>[] = [
 ];
 
 export default function PropertyTable() {
+  
   const id = useId();
   const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
   const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
@@ -318,6 +319,16 @@ export default function PropertyTable() {
   
     table.getColumn("gpsTagStatus")?.setFilterValue(newFilterValue.length ? newFilterValue : undefined);
   };
+
+
+  // MOUNT TO PREVENT USE CONTEXT ERROR DURING SSR BUILDS
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
   
 
   return (
